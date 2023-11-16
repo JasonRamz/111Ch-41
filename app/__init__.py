b82ab5f8fc0135170dd8dbf423c1ea3f921f7d19
Jason Ramirez <12ramirezjason@gmail.com>
2023-11-15 20:36:56 -0800

    Same from last class but with the sandbox.db

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 #-#- coding: utf-8 -#-
-"""Sample hello world from flask"""
+# """Sample hello world from flask"""
 
-from flask import Flask
-
-
-app = Flask(__name__)
-
-@app.route('/')                           # Flask decoreate to map routes to view functions
-
-def main():
-    return "Dr.Rafael is the best!!!"
+# from flask import Flask
 
 
-@app.route('/aboutme')                           # Flask decoreate to map routes to view functions
+# app = Flask(__name__)
+
+# @app.route('/')                           # Flask decoreate to map routes to view functions
+
+# def main():
+#     return "Dr.Rafael is the best!!!"
+
+
+# @app.route('/aboutme')                           # Flask decoreate to map routes to view functions
                                         # our function in flask is a view function
                                         # python dictionary .Dictionaries are key-value pairs.
-def intro():
-    me =["First Name: Jason Ramirez", "Last Name: Ramirez", "Hobbies:  Playing video games", "is_active: True"]
-    bullet_list = "".join(
-        "<li>%s</li>" %intro for intro in me
-    )
-    return "<ul>%s</ul>" % bullet_list
+# def intro():
+#     me =["First Name: Jason Ramirez", "Last Name: Ramirez", "Hobbies:  Playing video games", "is_active: True"]
+#     bullet_list = "".join(
+#         "<li>%s</li>" %intro for intro in me
+#     )
+#     return "<ul>%s</ul>" % bullet_list
 
                             
                             # flask when you return a dictionary if all 
